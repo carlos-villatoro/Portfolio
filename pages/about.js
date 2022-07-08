807022ca177e382e788efa1a838a9a38d714a462
carlos-villatoro <futurebound.carlosv@gmail.com>
2022-07-08 17:22:02 -0500

    preps for deploy

---
+++ pages/about.js
@@ -0,0 +1,41 @@
+import Image from "next/image"
+import styles from '../styles/About.module.css'
+
+export default function About(){
+    return (
+        <div>
+            <h1>About</h1>
+
+            <p className={[styles.extraGreen, styles.bigFont].join(' ')}>I am a programmer, much wow</p>
+            
+            <div>
+                <style jsx>{`
+                    .orangeText {
+                        color: yellow;
+                    }
+
+                    p{
+                        font-size: 70px
+                    }
+                `}</style>
+                <p className="orangeText">i want to play apex right now</p>
+
+            </div>
+
+            {/* loading an image locally */}
+            <Image 
+                // assumes you are in the public folder
+                src='/selfie.jpeg'
+                alt = 'myself'
+                width={900}
+                height={1600}
+            />
+            <Image 
+                src='https://placekitten.com/300/300'
+                alt='a kitten'
+                width={300}
+                height={300}
+            />
+        </div>
+    )
+}
---
+++ pages/users/[uid].jsx
@@ -0,0 +1,12 @@
+// import the use router hook to get the path params
+import { useRouter } from "next/router";
+
+export default function UserIdPage(){
+    // open instance of the router
+    const router = useRouter()
+    // pull the path param from the router
+    const{ uid } = router.query
+    return (
+        <h1>hello user at id {uid}</h1>
+    )
+}
